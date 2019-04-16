3f8b3f129bb5c8741ac45590f562e3c9ea77108a
jrvhulle <Jef.VanHulle@UGent.be>
2019-04-16 18:44:37 +0200

    Update BlockUrgencyScheduler.java

--- src/main/java/the/avengers/hospitalscheduler/urgencyschedulers/BlockUrgencyScheduler.java
+++ src/main/java/the/avengers/hospitalscheduler/urgencyschedulers/BlockUrgencyScheduler.java
@@ -18,33 +18,34 @@ import the.avengers.hospitalscheduler.primitives.Week;
  * @author user
  */
 public class BlockUrgencyScheduler extends BaseUrgencyScheduler {
-	public void fill(Week w, int nUrgencySlots) {
-    // There are minimum 10 urgent slots
-    if (nUrgencySlots == 10) {
-      for (int i = 0; i < w.days.length; i++) {
-            if (w.days[i].day == DayOfWeek.THURSDAY || w.days[i].day == DayOfWeek.SATURDAY) {
-                w.days[i].timeSlots[6].reservedForUrgent = true;
-            } else {
-                w.days[i].timeSlots[6].reservedForUrgent = true;
-                w.days[i].timeSlots[13].reservedForUrgent = true;
+    
+    public void fill(Week w, int nUrgencySlots) {
+        
+        // There are minimum 10 urgent slots
+        if (nUrgencySlots == 10) {
+            for (int i = 0; i < w.days.length; i++) {
+                if (w.days[i].day == DayOfWeek.THURSDAY || w.days[i].day == DayOfWeek.SATURDAY) {
+                    w.days[i].timeSlots[6].reservedForUrgent = true;
+                } else {
+                    w.days[i].timeSlots[6].reservedForUrgent = true;
+                    w.days[i].timeSlots[13].reservedForUrgent = true;
+                }
             }
-        }
         }
         
         //for 11 till 15, evenly distribute 3 slots for every whole day
-        
         if (nUrgencySlots > 10 && nUrgencySlots < 15) {
             int slots = 10;
             for (int i = 0; i < w.days.length; i++) {
                 if (w.days[i].day == DayOfWeek.THURSDAY || w.days[i].day == DayOfWeek.SATURDAY) {
-                w.days[i].timeSlots[6].reservedForUrgent = true;
-                } else if (nUrgencySlots - slots > 0){
-                w.days[i].timeSlots[6].reservedForUrgent = true;
-                w.days[i].timeSlots[13].reservedForUrgent = true;
-                w.days[i].timeSlots[20].reservedForUrgent = true;
+                    w.days[i].timeSlots[6].reservedForUrgent = true;
+                } else if (nUrgencySlots - slots > 0) {
+                    w.days[i].timeSlots[6].reservedForUrgent = true;
+                    w.days[i].timeSlots[13].reservedForUrgent = true;
+                    w.days[i].timeSlots[20].reservedForUrgent = true;
                 } else {
-                w.days[i].timeSlots[6].reservedForUrgent = true;
-                w.days[i].timeSlots[13].reservedForUrgent = true;    
+                    w.days[i].timeSlots[6].reservedForUrgent = true;
+                    w.days[i].timeSlots[13].reservedForUrgent = true;    
                 }
                 slots ++;
                 //all slots are used before sunday
@@ -53,27 +54,28 @@ public class BlockUrgencyScheduler extends BaseUrgencyScheduler {
         
         //from slot 15 till slot 16 (2nd slot on the half days)
         if (nUrgencySlots > 14 && nUrgencySlots < 17) {
-            for (int i = 0; i<w.days.length; i++ ){
+            for (int i = 0; i < w.days.length; i++ ) {
                 if (w.days[i].day == DayOfWeek.THURSDAY || w.days[i].day == DayOfWeek.SATURDAY) {
-                w.days[i].timeSlots[6].reservedForUrgent = true;
-                    if (nUrgencySlots == 16){
-                        w.days[i].timeSlots[13].reservedForUrgent = true;}
+                    w.days[i].timeSlots[6].reservedForUrgent = true;
+                    if (nUrgencySlots == 16) {
+                        w.days[i].timeSlots[13].reservedForUrgent = true;
+                    }
                 } else {
                     w.days[i].timeSlots[6].reservedForUrgent = true;
                     w.days[i].timeSlots[13].reservedForUrgent = true;
                     w.days[i].timeSlots[20].reservedForUrgent = true; 
                 }
-        }
+            }
         }
         
         //from slot 17 till slot 20
         if (nUrgencySlots > 16 && nUrgencySlots <= 20) {
             int slots = 16;
-            for (int i = 0; i<w.days.length; i++ ){
+            for (int i = 0; i < w.days.length; i++ ){
                 if (w.days[i].day == DayOfWeek.THURSDAY || w.days[i].day == DayOfWeek.SATURDAY) {
                     w.days[i].timeSlots[6].reservedForUrgent = true;
                     w.days[i].timeSlots[13].reservedForUrgent = true;
-                } else if (nUrgencySlots - slots > 0){
+                } else if (nUrgencySlots - slots > 0) {
                     w.days[i].timeSlots[6].reservedForUrgent = true;
                     w.days[i].timeSlots[13].reservedForUrgent = true;
                     w.days[i].timeSlots[20].reservedForUrgent = true;
@@ -84,7 +86,7 @@ public class BlockUrgencyScheduler extends BaseUrgencyScheduler {
                     w.days[i].timeSlots[20].reservedForUrgent = true;   
                 }
                 slots ++; 
-                }
             }
         }
+    }
 }
